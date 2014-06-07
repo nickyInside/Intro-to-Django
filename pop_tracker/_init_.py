4455506158e70e296ebf113391cc0cb195d8a39e
Nicky Setia <nicky.setia@gmail.com>
2014-06-07 17:55:59 -0400

    _init_.py

--- pop_tracker/_init_.py
+++ pop_tracker/_init_.py
@@ -1,14 +1,1 @@
-"""
-WSGI config for pop_tracker project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
-
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pop_tracker.settings")
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+ 
